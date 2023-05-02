da864af6f461c1e33de009095e286206c7c05eb8
Snow Yang <ytm199891@gmail.com>
2023-05-02 14:17:12 +0800

    add component: Add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-
 export default async function Home() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
-  const data = await res.json();
-
-  console.log(data);
   return (
     <>
-      <h1>{data.count}</h1>
-      <img src={data.results[2].url} width="500" height="600" alt="pokemon" />
+      <h1>Hokkaido</h1>
     </>
   );
 }
